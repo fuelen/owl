1a423e0cb0dfe15ef876baf4719c7840a16e4394
Artur Plysyuk <ifuelen@gmail.com>
2022-01-23 15:38:01 +0200

    Add examples with spinner widget

---
+++ examples/spinner.exs
@@ -0,0 +1,20 @@
+long_running_tasks =
+  [9000, 8000, 4000, 6000]
+  |> Enum.with_index(1)
+  |> Enum.map(fn {delay, index} ->
+    {fn -> Process.sleep(delay) end,
+     labels: [
+       processing: "##{index} - processing",
+       ok: "##{index} - completed",
+       error: "##{index} - error"
+     ]}
+  end)
+
+long_running_tasks
+|> Task.async_stream(
+  fn {long_running_task, opts} ->
+    Owl.Spinner.run(long_running_task, opts)
+  end,
+  timeout: :infinity
+)
+|> Stream.run()
