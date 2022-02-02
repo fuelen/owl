cf54a48a62a11632c42df5c05ffeb5da94a54116
Artur Plysyuk <ifuelen@gmail.com>
2022-02-03 00:44:19 +0200

    Rewrite `LiveScreenTest` with `CaptureIOFrames` helper

--- test/owl/live_screen_test.exs
+++ test/owl/live_screen_test.exs
@@ -1,46 +1,38 @@
 defmodule Owl.LiveScreenTest do
   use ExUnit.Case, async: true
-  import ExUnit.CaptureIO
+  import CaptureIOFrames
 
   @terminal_width 20
-  @sleep 10
-  @render_separator "#@₴?$0"
 
   test "server" do
-    renders =
-      capture_io(fn ->
-        {:ok, live_screen_pid} =
-          start_supervised({Owl.LiveScreen, terminal_width: @terminal_width, refresh_every: 10})
+    framese =
+      capture_io_frames(
+        fn live_screen_pid, render ->
+          Owl.LiveScreen.await_render(live_screen_pid)
+          IO.puts(live_screen_pid, "first\nput")
+          render.()
+          block1 = make_ref()
+          Owl.LiveScreen.add_block(live_screen_pid, block1, state: "First block:\nupdate #1")
+          render.()
+          IO.puts(live_screen_pid, "second\nput")
+          render.()
+          block2 = make_ref()
 
-        render = fn ->
-          GenServer.call(live_screen_pid, :render)
-          IO.write(@render_separator)
-        end
+          Owl.LiveScreen.add_block(live_screen_pid, block2, state: "Second block:\nupdate #1\n\n")
 
-        IO.puts(live_screen_pid, "first\nput")
-        render.()
-        block1 = make_ref()
-        Owl.LiveScreen.add_block(live_screen_pid, block1, state: "First block:\nupdate #1")
-        render.()
-        IO.puts(live_screen_pid, "second\nput")
-        render.()
-        block2 = make_ref()
+          render.()
+          IO.puts(live_screen_pid, "third\nput")
+          render.()
+          Owl.LiveScreen.update(live_screen_pid, block2, "Second block\nupdate #2")
+          Owl.LiveScreen.flush(live_screen_pid)
+          render.()
+          IO.puts(live_screen_pid, "new line")
+          IO.puts(live_screen_pid, "new line")
+        end,
+        terminal_width: @terminal_width
+      )
 
-        Owl.LiveScreen.add_block(live_screen_pid, block2, state: "Second block:\nupdate #1\n\n")
-
-        render.()
-        IO.puts(live_screen_pid, "third\nput")
-        render.()
-        Owl.LiveScreen.update(live_screen_pid, block2, "Second block\nupdate #2")
-        Owl.LiveScreen.flush(live_screen_pid)
-        Process.sleep(@sleep)
-        IO.write(@render_separator)
-        IO.puts(live_screen_pid, "new line")
-        IO.puts(live_screen_pid, "new line")
-        Owl.LiveScreen.stop(live_screen_pid)
-      end)
-
-    assert String.split(renders, @render_separator) == [
+    assert framese == [
              "first\nput\n\n",
              "\e[2KFirst block:\n\e[2Kupdate #1\n",
              "\e[1A\e[2K\e[1A\e[2K\e[1A\e[2Ksecond\nput\n\n\e[2KFirst block:\n\e[2Kupdate #1\n",
