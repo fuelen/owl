2d369320880a71ba4fefcf233e65449ac6e7155b
Artur Plysyuk <ifuelen@gmail.com>
2022-05-21 21:51:01 +0300

    Capture logs in test for `Owl.Task.run/1`

--- test/owl/task_test.exs
+++ test/owl/task_test.exs
@@ -1,8 +1,12 @@
 defmodule Owl.TaskTest do
   use ExUnit.Case, async: true
+  import ExUnit.CaptureLog
 
   test inspect(&Owl.Task.run/1) do
     assert {:ok, 1} = Owl.Task.run(fn -> 1 end)
-    assert {:exit, _} = Owl.Task.run(fn -> raise "boom" end)
+
+    assert capture_log(fn ->
+             assert {:exit, _} = Owl.Task.run(fn -> raise "boom" end)
+           end) =~ "(RuntimeError) boom"
   end
 end
