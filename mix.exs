c9a67decbf37df869527468220ed6babfce6c2d8
Artur Plysyuk <ifuelen@gmail.com>
2026-06-04 20:44:39 +0300

    Bump minimum Elixir to 1.16 and update CI matrix

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Owl.MixProject do
     [
       app: :owl,
       version: @version,
-      elixir: "~> 1.14",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
